419768a25a69beafb93101225616893a5c4365b1
Jeff Fowler <jeffowler@gmail.com>
2017-08-18 08:03:40 +0200

    remove now extraneous spaces

--- game_test.rb
+++ game_test.rb
@@ -3,57 +3,57 @@ require_relative "game"
 
 class GameTest < Test::Unit::TestCase
     def  test_no_score_game
-        game = make_game [0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0]
+        game = make_game [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         assert_equal game.score, 0
     end
 
     def  test_low_score_game
-        game = make_game [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]
+        game = make_game [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
         assert_equal game.score, 20
     end
 
     def  test_incomplete_with_spare
-        game = make_game [1,1, 6,4, 3]
+        game = make_game [1,1,6,4,3]
         assert_equal game.score, 18
     end
 
     def  test_mid_score_game
-        game = make_game [5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5]
+        game = make_game [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
         assert_equal game.score, 150
     end
 
     def  test_strike_tenth_frame
-        game = make_game [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 10, 10 ,9]
+        game = make_game [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,10,10,9]
         assert_equal game.score, 47
     end
 
     def  test_all_spares
-        game = make_game [7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 10]
+        game = make_game [7,3,7,3,7,3,7,3,7,3,7,3,7,3,7,3,7,3,7,3,10]
         assert_equal game.score, 173
     end
 
     def  test_perfect_game
-        game = make_game [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10]
+        game = make_game [10,10,10,10,10,10,10,10,10,10,10,10]
         assert_equal game.score, 300
     end
 
     def  test_incomplete_game
-        game = make_game [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  5]
+        game = make_game [10,10,10,10,10,10,10,10,10,10,5]
         assert_equal game.score, 280
     end
 
     def  test_extras
-        game = make_game [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10, 10, 10, 10]
+        game = make_game [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
         assert_equal game.score, 300
     end
 
     def  test_mostly_strikes
-        game = make_game [10,  10,  10,  0,0, 10,  10,  10,  10,  10,  10,  10,  10]
+        game = make_game [10,10,10,0,0,10,10,10,10,10,10,10,10]
         assert_equal game.score, 240
     end
 
     def  test_mostly_strikes_until_last_frame
-        game = make_game [10,  10,  10,  10,  10,  10,  10,  10,  10,  1,1]
+        game = make_game [10,10,10,10,10,10,10,10,10,1,1]
         assert_equal game.score, 245
     end
 
